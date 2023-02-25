f06505a0f9b9d816730e28d086aba72c46ed76e7
DuhPesky <jpescado@asu.edu>
2023-02-25 15:09:48 -0700

    changed telescope desc

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
   {
     'folke/trouble.nvim',
     keys = {
-      { '<leader>xx', '<cmd> TroubleToggle<CR>', desc = 'Toggle Trouble diagnostics window' }
+      { '<leader>xx', '<cmd> TroubleToggle<CR>', desc = 'Trouble: Toggle diagnostics window' }
     },
     opts = {
       use_diagnostic_signs = true,
